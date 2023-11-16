a36359046464c2d658092466f5e7c4bd5ca88152
lasakek <lasake13@gmail.com>
2023-11-16 09:20:10 +0100

    XML Read Feature

--- StreamLit_FTIR_deploy.py
+++ StreamLit_FTIR_deploy.py
@@ -124,15 +124,15 @@ def xml_to_data(uploaded_file):
                 except ValueError:
                     encoded_y_values = base64.b64decode(y_values_text)
                     y_values = struct.unpack(f'{len(encoded_y_values) // struct.calcsize("f")}f', encoded_y_values)
-                    st.write(y_values.shape)
-                    st.write(y_values.dtype)
-                    st.write(y_values)
+                    # st.write(y_values.shape)
+                    # st.write(y_values.dtype)
+                    # st.write(y_values)
 
                     # Split the string into a list of strings
-                    y_values = y_values.split(',')
+                    # y_values = y_values.split(',')
 
                     # Convert the list of strings to an array of integers
-                    y_values = array('i', map(int,y_values))
+                    y_values = array('f', y_values)
                     st.write("This is new shape: ", y_values)
 
             else:
