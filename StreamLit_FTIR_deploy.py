929f0fc3c02020fc7e56d8dd9234e979cfe0eaac
S-1-5-21-527763839-1561677997-902985232-110588 <lasake13@gmail.com>
2023-12-14 13:50:42 +0100

    New Baseline_substraction

--- StreamLit_FTIR_deploy.py
+++ StreamLit_FTIR_deploy.py
@@ -72,7 +72,7 @@ def WhittakerSmooth(x, w, lambda_, differences=1):
     background = spsolve(A, B)
     return np.array(background)
 
-def baseline_substraction(x, lambda_, porder, itermax=15):
+def baseline_substraction(x, lambda_=100, porder=4, itermax=15):
     '''
     Adaptive iteratively reweighted penalized least squares for baseline fitting
 
@@ -103,7 +103,7 @@ def baseline_substraction(x, lambda_, porder, itermax=15):
 
 
 #-------------Read in Data from .csv file---------------#
-def csv_to_data(uploaded_file, lambda_, porder_, base):
+def csv_to_data(uploaded_file):
     if uploaded_file is not None:
         file_names = []
         sample_names = []
@@ -125,8 +125,8 @@ def csv_to_data(uploaded_file, lambda_, porder_, base):
             except Exception as e:
                 st.warning("Please ensure the correct structure of the .csv file: \n x-values, y-values\n x1, y1\n x2, y2")
 
-            if base:
-                y_values = baseline_substraction(y_values, lambda_, porder_)
+
+            y_values = y_values - baseline_substraction(y_values)
 
             sample_name = f"Sample_{file_idx}"
 
@@ -150,7 +150,7 @@ def csv_to_data(uploaded_file, lambda_, porder_, base):
 
 
 #----------Reading in Data from .xml files---------------#
-def xml_to_data(uploaded_file, lambda_, porder_, base):
+def xml_to_data(uploaded_file):
     if uploaded_file is not None:
         file_idx = 0
         file_names = []
@@ -181,8 +181,8 @@ def xml_to_data(uploaded_file, lambda_, porder_, base):
                 st.error(f"Could not find Y-values in {file}", icon="🚨")
 
             # y_values = savgol_filter(y_values, window_length=15, polyorder=4)
-            if base:
-                y_values = baseline_substraction(y_values, lambda_, porder_)
+
+            y_values = y_values - baseline_substraction(y_values)
 
             fxv_tag = soup.find('parameter', {'name': 'FXV'})
             lxv_tag = soup.find('parameter', {'name': 'LXV'})
@@ -699,9 +699,6 @@ def main():
     # Define content for the Analysis Page (you can create another tab for analysis)
 
     #-----------------all of the Sidebar Input--------------------------#
-    st.session_state['lambda'] = st.slider("Lambda: ", 1,1000,10)
-    st.session_state['porder'] = st.slider("Porder", 1,10,1)
-    st.session_state['base'] = st.toggle("Turn on for baseline_correction")
     st.sidebar.header("Upload your FTIR Files here:")
 
 
@@ -710,12 +707,12 @@ def main():
 
     if not csv_toggle:
         uploaded_file = st.sidebar.file_uploader("Choose a file", type=['xml'], help='You are only able to upload .xml files.', accept_multiple_files=True)
-        sample_names, file_names, sample_objects = xml_to_data(uploaded_file, st.session_state['lambda'], st.session_state['porder'], st.session_state['base'])
+        sample_names, file_names, sample_objects = xml_to_data(uploaded_file)
         data_table = pd.DataFrame({"Sample Names": sample_names, "File Names": file_names})
     else:
         uploaded_file = st.sidebar.file_uploader("You are only able to upload .csv files with x-values in first columns and y-values in the second column.", type=['csv'],
                                                  accept_multiple_files=True)
-        sample_names, file_names, sample_objects = csv_to_data(uploaded_file, st.session_state['lambda'], st.session_state['porder'],st.session_state['base'])
+        sample_names, file_names, sample_objects = csv_to_data(uploaded_file)
         data_table = pd.DataFrame({"Sample Names": sample_names, "File Names": file_names})
        
     st.sidebar.markdown("**Your Data:**")
