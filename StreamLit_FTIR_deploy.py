a1a54eeee835f77521cd4219fa6eb2dfd46abecb
S-1-5-21-527763839-1561677997-902985232-110588 <lasake13@gmail.com>
2023-12-14 10:38:09 +0100

    New Baseline_substraction

--- StreamLit_FTIR_deploy.py
+++ StreamLit_FTIR_deploy.py
@@ -513,7 +513,7 @@ def peak_fit_lev(data, initial_guess, selected_samples, algorithm):
             convergence_tolerance = 1e-10
 
             # # Perform optimization
-            result = minimize(objective, initial_params_lev, bounds=bounds, method="L-BFGS",
+            result = minimize(objective, initial_params_lev, bounds=bounds, method="L-BFGS-B",
                               options={'maxiter': max_iterations, 'gtol': convergence_tolerance})
 
         else:
