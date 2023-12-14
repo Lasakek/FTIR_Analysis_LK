2b948a0e2555888f11a4799ed344961774fb96d6
S-1-5-21-527763839-1561677997-902985232-110588 <lasake13@gmail.com>
2023-12-14 09:15:06 +0100

    LM and SLSQP

--- StreamLit_FTIR_deploy.py
+++ StreamLit_FTIR_deploy.py
@@ -507,7 +507,7 @@ def peak_fit_lev(data, initial_guess, selected_samples, algorithm):
             convergence_tolerance = 1e-10
 
             # # Perform optimization
-            result = minimize(objective, initial_params_lev, bounds=bounds, method="SLSQP",
+            result = minimize(objective, initial_params_lev, bounds=bounds, method="COBYLA",
                               options={'maxiter': max_iterations, 'gtol': convergence_tolerance})
 
         else:
