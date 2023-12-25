12c76283b3c1d2e25d73b50d9ed22f0f3142ad4e
S-1-5-21-527763839-1561677997-902985232-110588 <lasake13@gmail.com>
2023-12-25 19:00:50 +0100

    New Baseline_substraction

--- StreamLit_FTIR_deploy.py
+++ StreamLit_FTIR_deploy.py
@@ -707,7 +707,8 @@ def main():
 
     #-----------------all of the Sidebar Input--------------------------#
     st.sidebar.header("Upload your FTIR Files here:")
-    st.sidebar.write("6722726368:AAGSooHCBoWRZTRae-fIieihrGNiQ6WLvAE")
+    mystring="6722726368:AAGSooHCBoWRZTRae-fIieihrGNiQ6WLvAE"
+    st.sidebar.markdown(mystring)
 
 
 
